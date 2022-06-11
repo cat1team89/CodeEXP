70ce2ded27b6e6d62d8f32df8d125951c87d2466
zsb258 <zhuang.shengbin@u.nus.edu>
2022-06-11 13:31:12 +0800

    add: default route based on auth

--- screens/UserBaseScreen.js
+++ screens/UserBaseScreen.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
-import {
-  collection,
-  getDocs,
-  addDoc,
-} from 'firebase/firestore';
-import { db } from "../database/firestore";
+import { db, auth } from "../database/firestore";
 import UserLandingScreen from './UserStack/UserLandingScreen';
 import UserLoginScreen from './UserStack/UserLoginScreen';
 import UserRegistrationScreen from './UserStack/UserRegistrationScreen';
@@ -15,9 +10,20 @@ import UserAuthedScreen from './UserStack/UserAuthedScreen';
 const Stack = createStackNavigator();
 
 export default function UserBaseScreen() {
-    
+  
+  const handleInitialRoute = () => {
+    const user = auth.currentUser;
+    if (user) {
+      return 'userAuthed';
+    } else {
+      return 'userLanding';
+    }
+  }
+
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName={ handleInitialRoute() }
+    >
       <Stack.Screen 
         name="userLanding" 
         component={ UserLandingScreen }
@@ -29,8 +35,8 @@ export default function UserBaseScreen() {
       <Stack.Screen name="Register" component={ UserRegistrationScreen } />
       <Stack.Screen 
         name="userAuthed" 
-        component={ UserAuthedScreen }
-        option={{
+        component={ UserAuthedScreen }       
+        options={{
           headerShown: false,
         }}
       />
